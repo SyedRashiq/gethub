ae21b33abc1e4a73b35a5f0104df003033edddc4
Syed Rashiq <rashiq.shah29@gmail.com>
2019-10-09 10:17:50 +0500

    ghhyjjukj

--- app/src/main/java/com/example/gethub/MainActivity.java
+++ app/src/main/java/com/example/gethub/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.gethub;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ThemedSpinnerAdapter;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView,textView2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ mylibrary/src/main/java/com/example/mylibrary/Helper0.java
@@ -0,0 +1,11 @@
+package com.example.mylibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Helper0 {
+    Context context;
+    public  void add(){
+        Toast.makeText(context, "i am ali", Toast.LENGTH_SHORT).show();
+    }
+}
